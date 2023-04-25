69ea99121ffc807a33d5aa5d14b3b323452a7322
t.irie <tak.mibp@gmail.com>
2023-04-25 13:30:15 +0900

    add button to back home

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,12 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import { Link } from "gatsby"
 
 const AboutPage: React.FC<PageProps> = () => {
   return (
     <main>
       <h1>hey, it's about</h1>
+      <Link to="/">Back to Home</Link>
     </main>
   )
 }
