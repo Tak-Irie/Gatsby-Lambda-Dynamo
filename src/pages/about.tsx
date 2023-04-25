e98e6f07a69ed3a4cbfe175bd6bbc19d992f11a2
t.irie <tak.mibp@gmail.com>
2023-04-25 15:08:17 +0900

    add pictures

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+
 import { Layout } from "../components/layout";
 
 const AboutPage: React.FC<PageProps> = () => {
 	return (
 		<Layout pageTitle="About Page">
 			<p>hey, it's about.</p>
+			<StaticImage alt="shiba-inu" src="../images/shiba.webp" />
 		</Layout>
 	);
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 import { Layout } from "../components/layout";
 
@@ -7,6 +8,10 @@ const IndexPage: React.FC<PageProps> = () => {
 	return (
 		<Layout pageTitle="Home Page">
 			<p>hey, it's home.</p>
+			<StaticImage
+				alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+				src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+			/>
 		</Layout>
 	);
 };
