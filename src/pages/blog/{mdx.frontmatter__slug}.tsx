9d0ac61b1e6f988af3acbd58824f11a22dfdf86b
t.irie <tak.mibp@gmail.com>
2023-04-25 18:54:51 +0900

    add types

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
+import type { PageProps } from "gatsby";
+
 import { Layout } from "../../components/layout";
 import { Seo } from "../../components/seo";
 
-const BlogPost = () => {
+const BlogPost: React.FC<PageProps> = () => {
 	return (
 		<Layout pageTitle="Super Cool Blog Posts">
 			<p>My blog post contents will go here (eventually).</p>
